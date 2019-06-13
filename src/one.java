faef1ace14d7aa9d31d173afe64b3488babcdb25
zhangchaowen <2423968859@qq.com>
2019-06-13 11:59:39 +0800

    one

---
+++ src/one.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhangchaowen
+ * Date: 2019/06/13
+ * Description:
+ * Version: V1.0
+ */
+public class one {
+    public static void main(String[] args) {
+        System.out.println("hh");
+    }
+}
